46bce697dfd0ee632e39457b454322baec7dd75e
Zain <reply2zain@gmail.com>
2023-05-13 14:35:21 -0400

    refactor: bump version to 1.0.4

--- commands/client/version.py
+++ commands/client/version.py
@@ -4,6 +4,6 @@ from lib.MessageEventLocal import MessageEventLocal
 
 
 async def execute(event: MessageEventLocal):
-    await event.message.channel.send("version: 1.0.3")
+    await event.message.channel.send("version: 1.0.4")
 
 
