ef77f0d1ec7dbb1fb940133e1f58bd274db74c7e
Zain <reply2zain@gmail.com>
2023-05-14 11:35:03 -0400

    fix: help list formatting

--- commands/client/help/help.py
+++ commands/client/help/help.py
@@ -2,14 +2,16 @@ import discord
 
 from lib.MessageEventLocal import MessageEventLocal
 
+DESCRIPTION = f"""
+`track [voice-channel]` - tracks a voice channel and sends a notification when the first person joins
+
+`untrack [voice-channel]` - untracks a voice channel
+"""
+
 async def execute(event: MessageEventLocal):
     embed = discord.Embed(
         title="Commands",
-        description=f"""
-        `track [voice-channel]` - tracks a voice channel and sends a notification when the first person joins
-        `untrack [voice-channel]` - untracks a voice channel
-        """
-        ,
+        description=DESCRIPTION,
         color=0x00FF00,
     )
     
