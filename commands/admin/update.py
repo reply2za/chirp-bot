ab8b7ab67fb60c9dc4ff782a4550ad00bef0ad26
Zain <reply2zain@gmail.com>
2023-05-14 11:56:31 -0400

    fix: update cmd

--- commands/admin/update.py
+++ commands/admin/update.py
@@ -5,7 +5,8 @@ import subprocess
 BASH_CMD = "git stash && git pull && pm2 restart chirp-bot"
 async def execute(event: MessageEventLocal):
     await event.message.channel.send("updating...")
-    process = subprocess.Popen(BASH_CMD.split(), stdout=subprocess.PIPE)
+    print('running bash command: ', BASH_CMD)
+    process = subprocess.Popen(BASH_CMD, stdout=subprocess.PIPE, shell=True)
     output, error = process.communicate()
     print('output: ', output)
     print('error: ', error)
