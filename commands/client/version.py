bef5bfbd541e7f50eba5263c2bec7ce3d96be456
Zain <reply2zain@gmail.com>
2023-05-14 11:34:43 -0400

    refactor: use pyproject.toml for version

--- commands/client/version.py
+++ commands/client/version.py
@@ -4,6 +4,12 @@ from lib.MessageEventLocal import MessageEventLocal
 
 
 async def execute(event: MessageEventLocal):
-    await event.message.channel.send("version: 1.0.8")
+    # get the version number from the pyproject.toml file
+    with open("pyproject.toml", "r") as f:
+        for line in f.readlines():
+            if "version" in line:
+                version = line.split("=")[1].strip().replace('"', "")
+                break
+    await event.message.channel.send(f"version: {version}")
 
 
