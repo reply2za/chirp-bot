ad250f77757cc67cd2a2128fbecdea57138dd848
Zain <reply2zain@gmail.com>
2023-05-19 17:47:32 -0400

    refactor: auto update version from pyproject.toml

--- commands/client/version.py
+++ commands/client/version.py
@@ -1,15 +1,17 @@
 
 
+from lib.logger import logger
 from lib.MessageEventLocal import MessageEventLocal
 
+with open("pyproject.toml", "r") as f:
+    version = None
+    for line in f.readlines():
+        if "version" in line:
+            version = line.split("=")[1].strip().replace('"', "")
+            break
+    if version is None:
+        logger.error("version not found in pyproject.toml")
 
 async def execute(event: MessageEventLocal):
     # get the version number from the pyproject.toml file
-    with open("pyproject.toml", "r") as f:
-        for line in f.readlines():
-            if "version" in line:
-                version = line.split("=")[1].strip().replace('"', "")
-                break
     await event.message.channel.send(f"version: {version}")
-
-
